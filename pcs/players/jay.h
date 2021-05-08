b414471eeb3137056d3df461841ca8508e077622
Timothy Rice <timothy.rice@unimelb.edu.au>
2021-05-08 15:09:08 +1000

    Add Beats for A Nightmare at Hill Manor

--- pcs/players/jay.h
+++ pcs/players/jay.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #define PLAYER_NAME "Jay"
-#define MUNDANE_XP  2
-#define ARCANE_XP   0
+#define MUNDANE_BEATS   2
+#define MUNDANE_XP      2
+#define ARCANE_XP       0
--- pcs/players/michael.h
+++ pcs/players/michael.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #define PLAYER_NAME "Michael"
-#define MUNDANE_XP  2
-#define ARCANE_XP   0
+#define MUNDANE_BEATS   2
+#define MUNDANE_XP      2
+#define ARCANE_XP       0
