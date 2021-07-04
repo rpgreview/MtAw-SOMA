6c02bbf115157cf5043dcdab57a18e0f9adb9beb
Timothy Rice <timothy.rice@unimelb.edu.au>
2021-07-05 07:54:47 +1000

    Give Andrew another Beat

--- characters/include/players/andrew.h
+++ characters/include/players/andrew.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define PLAYER_NAME "Andrew"
-#define MUNDANE_BEATS   4
-#define MUNDANE_XP      7
+#define MUNDANE_BEATS   0
+#define MUNDANE_XP      8
 #define ARCANE_BEATS    4
 #define ARCANE_XP       3
